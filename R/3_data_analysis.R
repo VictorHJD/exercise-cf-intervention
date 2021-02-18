d15aae87bb97c140df3b3f60ca6358a84c575119
Jarquín-Díaz, Víctor Hugo <victor-hugo.jarquin-diaz@charite.de>
2021-02-18 17:59:34 +0100

    Data analysis: Alpha diversity estimations

---
+++ R/3_data_analysis.R
@@ -0,0 +1,111 @@
+##Cystic-Fibrosis Microbiome Project (Mainz)
+##Data analysis
+##Víctor Hugo Jarquín-Díaz 18.02.2021
+
+##To phyloseq
+library(phyloseq)
+library(microbiome)
+library(tidyverse)
+library(data.table)
+library(knitr)
+
+##Load data 
+if(!exists("PS")){
+  if(isTRUE(reRun)){
+    source("R/2_phyloseq_preparation.R") ## Run the script at base directory of repository!   
+  } else {
+    metadata<- readRDS(file = "~/CF_project/exercise-cf-intervention/data/PhyloSeqComp.Rds") ##New annotation SILVA
+  }
+}
+
+##Have a look into the data
+summarize_phyloseq(PS) ##Non-Normalized
+
+## HOW many ASVs for off-target eukaryotes and archaea
+table(tax_table(PS)[, "Kingdom"], exclude = NULL) ## ---> README results summary
+
+## HOW many reads for off-target eukaryotes and archaea
+by((otu_table(PS)), tax_table(PS)[, "Kingdom"], sum) ## --->  README results summary
+
+##Negative controls are way bellow the samples, let's remove them
+
+PS1 <- prune_samples(sample_sums(PS)>190, PS)
+
+##Filtering 
+hist(sample_sums(PS1))
+
+##1) Taxa filtering: Remove "NA" ASVs
+PS2<- subset_taxa(PS1, !is.na(Phylum) & !Phylum %in% c(""))
+
+##2) Taxa filtering: Remove low prevalent taxa
+##Create a prevalence dataframe 
+Prevdf<- apply(X = otu_table(PS2),
+               MARGIN = 1,
+               FUN = function(x){sum(x > 0)})
+
+##Add taxonomy and total read counts to this data.frame
+Prevdf<- data.frame(Prevalence = Prevdf,
+                    TotalAbundance = taxa_sums(PS2),
+                    tax_table(PS2))
+
+plyr::ddply(Prevdf, "Phylum", function(df1){
+  data.frame(mean_prevalence=mean(df1$Prevalence),total_abundance=sum(df1$TotalAbundance,na.rm = T),stringsAsFactors = F)
+})
+
+ggplot(Prevdf, aes(TotalAbundance, Prevalence / nsamples(PS2),color=Phylum)) +
+  geom_hline(yintercept = 0.05, alpha = 0.5, linetype = 2) + geom_point(size = 2, alpha = 0.7) +
+  scale_x_log10() +  xlab("Log 10 Total Reads") + ylab("Prevalence [Prop. of Samples]") +
+  theme_bw()+
+  facet_wrap(~Phylum) + theme(legend.position="none")
+
+##3) Transform to even sampling depth
+## Rarefy without replacement
+##PS3<- rarefy_even_depth(PS2, rngseed=2020, sample.size=min(sample_sums(PS2)), replace=F)
+##readcount(PS3)
+
+## Merge ASVs that have the same taxonomy at a certain taxonomic rank (in this case Phylum and Family)
+PS.Fam<-  tax_glom(PS2, "Family", NArm = F)
+summarize_phyloseq(PS.Fam)
+
+PS.Gen<-  tax_glom(PS2, "Genus", NArm = T)
+summarize_phyloseq(PS.Gen)
+
+PS.Phy<-  tax_glom(PS2, "Phylum", NArm = F)
+summarize_phyloseq(PS.Phy)
+
+plot_bar(PS.Phy, fill="Phylum") + facet_wrap(~material, scales= "free_x", nrow=1)
+
+##Alpha diversity (rarefied)
+plot_richness(PS2, x= "material", color = "sex" , measures = c("Observed","Chao1", "Shannon")) +
+  geom_boxplot()+
+  geom_jitter(alpha= 0.005)+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle=90))
+
+alphaDiv <-microbiome::alpha(PS2, index = "all")
+kable(head(alphaDiv))
+
+alphadiv%>%
+  rownames_to_column()->tmp1
+
+as.tibble(sample)%>%
+  mutate(rowname= paste0("Sample", 1:nrow(sample)))->tmp2
+
+tmp1<-inner_join(tmp1, tmp2, by="rowname")
+rownames(tmp1)<- tmp1$rowname
+tmp1$rowname<- NULL
+alphadiv<- tmp1
+rm(tmp1,tmp2)
+
+##Estimate global indicators
+
+##Testing for differences in alpha diversity
+# Construct the data
+sdt <- meta(PS1)
+sdt$diversity <- diversities(PS1, "shannon")$diversity_shannon
+# Split the values by group
+spl <- split(d$diversity, d$sex)
+# Kolmogorov-Smironv test
+pv <- ks.test(spl$female, spl$male)$p.value
+# Adjust the p-value
+padj <- p.adjust(pv)
